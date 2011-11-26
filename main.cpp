982ba1367eee9094780e71e5f0bc93fc73b5bb5b
ftg <ftg@lutix.org>
2011-11-26 18:28:16 +0100

    Ajout d'un message handler

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <gloox/client.h>
 #include <gloox/connectionlistener.h>
+#include <gloox/messagehandler.h>
 #include <iostream>
 #include <string>
 
 using namespace gloox;
 
-class MyClass : public ConnectionListener
+class MyClass : public ConnectionListener,MessageHandler
  {
 public:
+// à la connexion
 void doSomething();
 void onConnect();
 void onDisconnect(ConnectionError e);
 bool onTLSConnect(const CertInfo& info);
+
+//à la reception de messages
+void handleMessage(const Message& msg, MessageSession* session = 0);
 };
 
 void MyClass::doSomething()
@@ -19,6 +24,7 @@ void MyClass::doSomething()
 	JID jid( "bot@lutix.org/cptalk" );
 	Client* client = new Client( jid, "fab99999" );
 	client->registerConnectionListener( this );
+	client->registerMessageHandler( this );
 	client->connect();
 }
 
@@ -35,10 +41,15 @@ void MyClass::onDisconnect(ConnectionError e)
 
 bool MyClass::onTLSConnect(const CertInfo& info)
 		 {
-				 std::cout << "connecté en TLS!" << std::endl;			//             // presence info
+				 //std::cout << "connecté en TLS!" << std::endl;			//             // presence info
 				//             // presence info
 		}
 
+void MyClass::handleMessage(const Message& msg, MessageSession* session)
+{
+
+		std::cout << "un message a été reçu!" << std::endl;
+}
 
 int main() {
 
