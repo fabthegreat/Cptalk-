ba2134f3c95fa4782cfc9b54fd42dde255cf2ebe
ftg <ftg@lutix.org>
2011-12-11 15:50:17 +0100

    Ajout du controle des fleches d'historique sur l'input, up and down

--- gui.cpp
+++ gui.cpp
@@ -16,74 +16,22 @@ entree.se_dessiner();
 //sortie.affichage();
 while(true){
 
-sortie.refresh();
-entree.refresh();
+	sortie.refresh();
+	entree.refresh();
 
-entree.editer(sortie,true);
-entree.raz();
-sortie.raz();
-sortie.affichage();
+	entree.editer(sortie,true);
+	entree.raz();
+	sortie.raz();
+	sortie.affichage();
 
-update_panels();
-//doupdate();
+	update_panels();
+	//doupdate();
 }
 
 
-getch();
-
+endwin();
 return 0;
 
 }
 
-
-
-void test_c(){		
-WINDOW *fenetres[2];
-PANEL *panels[2];
-char commande[1000];
-
-
-int max_lin,max_col;
-
-initscr();
-cbreak();
-echo();
-
-getmaxyx(stdscr,max_lin,max_col);
-
-// newwin(nb_ligne,nb_colonnes,depart_ligne,depart_colonne);
-fenetres[0]=newwin(max_lin-3,max_col,0,0);
-fenetres[1]=newwin(3,max_col,max_lin-3,0);
-
-
-//wborder(fenetres[0],' ',' ',' ','#',' ',' ',' ',' ');
-box(fenetres[0],0,0);
-box(fenetres[1],0,0);
-
-panels[0]= new_panel(fenetres[0]);
-panels[1]= new_panel(fenetres[1]);
-
-
-
-mvwprintw(fenetres[1],1,1,"> ");
-
-
-update_panels();
-
-doupdate();
-
-
-while(true){
-
-wrefresh(fenetres[0]);
-mvwgetstr(fenetres[1],1,3,commande);
-mvwprintw(fenetres[0],1,1,"%s",commande);
-
-
-}
-
-getch();
-
-endwin();
-}
 
